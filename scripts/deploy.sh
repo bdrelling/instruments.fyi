228e2de608a85fcabe6086117d8f72223e50b75d
Brian Drelling <brian@drelling.com>
2022-09-05 18:41:44 -0500

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Trigger a deployment.
 gh workflow run Deploy
+
+# If we pass the "--watch" flag to the script, we'll attempt to watch the run.
+if [ "$1" == "--watch" ]; then
+    # We need to sleep for a few seconds to give the deployment time to resolve.
+    sleep 3
+
+    # Get the latest run ID from the list.
+    run_id=$(gh run list --limit 1 --workflow Deploy --json url | grep -Eow "[0-9]+")
+
+    # Start watching the latest run.
+    gh run watch $run_id
+fi
